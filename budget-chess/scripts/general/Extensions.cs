f7d42bcbd99b23141763665f51bec7129fea406d
Kumodatsu <dragonkumo@gmail.com>
2022-08-03 08:12:51 +0200

    Add list extensions

--- budget-chess/scripts/general/Extensions.cs
+++ budget-chess/scripts/general/Extensions.cs
@@ -1,6 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
 namespace BudgetChess {
 
-  static class MultidimensionalArrayExtensions {
+  public static class MultidimensionalArrayExtensions {
     public static bool SequenceEquals<T>(this T[,] xs, T[,] ys) {
       if (xs.GetLength(0) != ys.GetLength(0)
           || xs.GetLength(1) != ys.GetLength(1)) {
@@ -13,6 +17,23 @@ namespace BudgetChess {
             return false;
 
       return true;
+    }
+  }
+
+  public static class ListExtensions {
+    public static void Filter<T>(this List<T> xs, Predicate<T> p) {
+      for (int i = xs.Count - 1; i >= 0; i--)
+        if (!p(xs[i]))
+          xs.RemoveAt(i);
+    }
+
+    public static void Print<T>(this List<T> xs) {
+      var sb = new StringBuilder();
+      sb.AppendLine("==== List ====");
+      foreach (T x in xs)
+        sb.AppendLine(x.ToString());
+      sb.AppendLine("==============");
+      Godot.GD.Print(sb.ToString());
     }
   }
 
